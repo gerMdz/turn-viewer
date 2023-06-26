dd4cb95e8cce47211cf9f233489193cdb09cb4f4
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2023-06-26 20:50:11 -0300

    :art: Iniciando las entidades

--- src/Entity/Turno.php
+++ src/Entity/Turno.php
@@ -3,16 +3,20 @@
 namespace App\Entity;
 
 use App\Repository\TurnoRepository;
-use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Uid\Uuid;
+use Doctrine\ORM\Mapping as ORM;
+
+
 
 #[ORM\Entity(repositoryClass: TurnoRepository::class)]
 class Turno
 {
     #[ORM\Id]
-    #[ORM\CustomIdGenerator]
     #[ORM\Column(type: 'uuid', unique: true)]
-    private $id;
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id;
 
     #[ORM\Column(type: 'string', length: 255)]
     private ?string $state;
